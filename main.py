9dc70168c2285dc7e44a4ed6814bd4509681ee77
Sergey Kostyaev <kostyaev.sergey2@wb.ru>
2024-06-20 03:58:14 +0700

    Add docstrings

--- main.py
+++ main.py
@@ -11,22 +11,73 @@ model.eval()
 app = FastAPI()
 
 class Document(BaseModel):
+    """
+    A model representing a document with an ID and text.
+
+    Attributes:
+        id (conint, gt=0): The unique ID of the document.
+        text (str): The text content of the document.
+    """
+
     id: conint(gt=0)
     text: str
 
 class RequestData(BaseModel):
+    """
+    A model representing a request to rerank documents based on their
+    similarity to a query.
+
+    Attributes:
+        query (str): The query string used for comparison.
+        documents (List[Document]): A list of Document objects to be
+        ranked.
+
+    Methods:
+        construct_pairs(): Returns a list of pairs, where each
+        pair consists of the query and a document's text.
+    """
+
     query: str
     documents: List[Document]
 
     def construct_pairs(self):
+        """
+        Constructs pairs of query and document texts for scoring.
+
+        Returns:
+            A list of pairs, where each pair consists of the
+            query and a document's text.
+
+        """
         return [[self.query, doc.text] for doc in self.documents]
 
 class ResponseData(BaseModel):
+    """
+    A model representing the response to a reranking request.
+
+    Attributes:
+        id (conint, gt=0): The ID of the ranked document.
+        similarity (float): The calculated similarity score between the query
+        and the document's text.
+    """
     id: conint(gt=0)
     similarity: float
 
 @app.post("/api/v1/rerank")
 async def rerank_documents(request: RequestData):
+    """
+    Ranks a list of documents based on their similarity to a given query using
+    sequence classification.
+
+    Args:
+        request (RequestData): A RequestData object containing the query and
+        a list of Document objects.
+
+    Returns:
+        A ResponseData object containing the ranked documents with their
+        corresponding similarity scores.
+    """
+
     response = []
     pairs = request.construct_pairs()
     with torch.no_grad():
